c1339f1f292935a1932a019eed33faef5ae53488
aetherspritee <schautendustin@gmail.com>
2023-12-24 15:25:48 +0100

    unifing senet_models files from msirs/msirs-utils

--- segmentation/senet_model.py
+++ segmentation/senet_model.py
@@ -116,6 +116,50 @@ class SENet:
         """
         return self.get_descriptor(img)
 
+    def heatmap(self, img: np.ndarray):
+        img = self.prep_image(img)
+        last_layer_weights = self.model.layers[-3].get_weights()[0]
+
+        model2 = tf.keras.models.Model(
+            inputs=self.model.input,
+            outputs=(self.model.layers[-8].output, self.model.layers[-3].output),
+        )
+        last_conv_layer_output, last_layer_output = model2.predict(img)
+
+        last_conv_layer_output = np.squeeze(last_conv_layer_output)
+        pred = np.argmax(last_layer_output)
+        h = int(img.shape[1] / last_conv_layer_output.shape[1])
+        # w = int(img.shape[2] / last_conv_layer_output.shape[2])
+        # upsampled_heat = scipy.ndimage.zoom(
+        #     last_conv_layer_output, (h, h, 1), order=1
+        # )
+        upsampled_heat = skimage.transform.resize(
+            last_conv_layer_output,
+            (
+                last_conv_layer_output.shape[0] * h,
+                last_conv_layer_output.shape[1] * h,
+                512,
+            ),
+            anti_aliasing=True,
+        )
+        weights_for_pred = last_layer_weights[pred]
+        # weights_p1 = weights_for_pred[:256]
+        # print(np.shape(weights_p1))
+
+        # TODO: upsample the last dim for conv layer to make it compatable
+        # heatmap = np.dot(upsampled_heat.reshape((224 * 224, 256)), weights_p1).reshape(
+        #     224, 224
+        # )
+
+        heatmap = np.dot(
+            upsampled_heat.reshape((224 * 224, 512)), weights_for_pred
+        ).reshape(224, 224)
+
+        fig, ax = plt.subplots()
+        ax.imshow(np.squeeze(img))
+        ax.imshow(heatmap, cmap="jet", alpha=0.5)
+        plt.show()
+
     @staticmethod
     def prep_image(img: np.ndarray) -> np.ndarray:
         """
@@ -139,24 +183,27 @@ if __name__ == "__main__":
         "/Users/dusc/codebase-v1/data/data/test/cra/B07_012260_1447_XI_35S194W_CX4750_CY4036.jpg",
         "/Users/dusc/codebase-v1/data/data/test/ael/P06_003352_1763_XN_03S345W_CX440_CY3513.jpg",
         "/Users/dusc/codebase-v1/data/data/test/cra/K01_053719_1938_XI_13N232W_CX1714_CY6640.jpg",
+        "/Users/dusc/codebase-v1/data/data/test/cli/F08_038957_1517_XN_28S040W_CX727_CY4635.jpg",
+        "/Users/dusc/codebase-v1/data/data/test/fss/B07_012259_1421_XI_37S167W_CX4172_CY5294.jpg",
     ]
 
     features = []
 
     for i in range(len(img_paths)):
-        features.append(model.get_descriptor(skimage.io.imread(img_paths[i])))
+        # features.append(model.get_descriptor(skimage.io.imread(img_paths[i])))
         print(model.predict(skimage.io.imread(img_paths[i])))
+        model.heatmap(img=skimage.io.imread(img_paths[i]))
 
-    dis01 = scipy.spatial.distance.cosine(features[0], features[1])
-    dis02 = scipy.spatial.distance.cosine(features[0], features[2])
-    dis03 = scipy.spatial.distance.cosine(features[0], features[3])
-    dis13 = scipy.spatial.distance.cosine(features[1], features[3])
-    dis12 = scipy.spatial.distance.cosine(features[1], features[2])
-    dis10 = scipy.spatial.distance.cosine(features[1], features[0])
+    # dis01 = scipy.spatial.distance.cosine(features[0], features[1])
+    # dis02 = scipy.spatial.distance.cosine(features[0], features[2])
+    # dis03 = scipy.spatial.distance.cosine(features[0], features[3])
+    # dis13 = scipy.spatial.distance.cosine(features[1], features[3])
+    # dis12 = scipy.spatial.distance.cosine(features[1], features[2])
+    # dis10 = scipy.spatial.distance.cosine(features[1], features[0])
 
-    print(f"{dis01 = }")
-    print(f"{dis02 = }")
-    print(f"{dis03 = }")
-    print(f"{dis10 = }")
-    print(f"{dis12 = }")
-    print(f"{dis13 = }")
+    # print(f"{dis01 = }")
+    # print(f"{dis02 = }")
+    # print(f"{dis03 = }")
+    # print(f"{dis10 = }")
+    # print(f"{dis12 = }")
+    # print(f"{dis13 = }")
